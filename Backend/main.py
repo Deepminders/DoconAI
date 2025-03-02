0d5abaede779dfb1deee0f80285798fd6fa50229
Sehara2002 <fernandomsa.22@uom.lk>
2025-03-02 13:59:55 +0530

    Removing Sample

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,8 @@
 from Config.db import client
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from Routes.SampleRoutes import router as SampleRouter
 from Routes.UserRoutes import router as UserRouter
 app = FastAPI()
 app.add_middleware(CORSMiddleware)
 
-# app.include_router(SampleRouter)
 app.include_router(UserRouter)
