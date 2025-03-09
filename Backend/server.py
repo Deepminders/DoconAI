f76d12acf2695870be579b231a707fa08ed1e157
laavanjan <130158996+laavanjan@users.noreply.github.com>
2025-03-09 18:25:05 +0530

    added projectrouter to server.py

--- Backend/server.py
+++ Backend/server.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from Routes.UserRoutes import router
+from Routes.ProjectRoutes import routerproject
 from Routes.DocumentRoutes import router as doc_router
 from Routes import StaffRoutes
 
 server = FastAPI()
 server.include_router(router)
 server.include_router(doc_router)
+server.include_router(routerproject)
 server.include_router(StaffRoutes.router)
 
 @server.get("/home")
