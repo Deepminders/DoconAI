3dbbaf62a1854f19f3ec3e17f651b301d73aaf71
laavanjan <130158996+laavanjan@users.noreply.github.com>
2025-03-09 18:13:38 +0530

    Added ProjectRoutes.py and relevant codes into repository also remove necessary comments

--- Backend/Controllers/ProjectController.py
+++ Backend/Controllers/ProjectController.py
@@ -7,15 +7,7 @@ from fastapi import HTTPException
 
 
 async def add_project(project: ProjectModel) -> dict:
-    """
-    Adds a new project to the database.
 
-    Args:
-        project: ProjectModel object containing project data.
-
-    Returns:
-        A dictionary indicating the success or failure of the operation.
-    """
     new_project = project.to_mongo_dict()  # Convert date fields
     try:
         result = await project_collection.insert_one(new_project)
@@ -31,25 +23,12 @@ async def add_project(project: ProjectModel) -> dict:
         }
 
 async def get_projects() -> dict:
-    """
-    Retrieves all projects from the database.
 
-    Returns:
-        A dictionary containing the list of projects.
-    """
     project_cursor = project_collection.find()
     return await getAllProject(project_cursor)
 
 async def find_project(id: ObjectId) -> dict:
-    """
-    Finds a project by its ID.
 
-    Args:
-        id: The ObjectId of the project to find.
-
-    Returns:
-        A dictionary containing the project details or an error message.
-    """
     project = await project_collection.find_one({"_id": id})
     if project:
         return {
@@ -62,19 +41,7 @@ async def find_project(id: ObjectId) -> dict:
         }
 
 async def update_project(project_id: str, project: ProjectUpdateModel):
-    """
-    Updates a project by its ID.
 
-    Args:
-        project_id: The ID of the project to update.
-        project: The Project object containing the updated data.
-
-    Returns:
-        A dictionary containing the update message and updated fields.
-
-    Raises:
-        HTTPException: If the project_id is invalid, no valid fields are provided, or the project is not found.
-    """
     try:
         object_id = ObjectId(project_id)  # Convert project_id to ObjectId
     except:
@@ -99,18 +66,7 @@ async def update_project(project_id: str, project: ProjectUpdateModel):
     return {"message": "Project updated successfully", "updated_fields": update_data}
 
 async def delete_project(project_id: str):
-    """
-    Deletes a project by its ID.
 
-    Args:
-        project_id: The ID of the project to delete.
-
-    Returns:
-        A dictionary containing the deletion message.
-
-    Raises:
-        HTTPException: If the project_id is invalid or the project is not found.
-    """
     try:
         # Convert project_id from string to ObjectId
         object_id = ObjectId(project_id)
