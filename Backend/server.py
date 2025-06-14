08a74105fe87c5e091502cf1626fe7aa4d243972
lahiruni23 <lahihansi2323@gmail.com>
2025-06-14 20:22:16 +0530

    Login ui and signup UI

--- Backend/server.py
+++ Backend/server.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from Routes.UserRoutes import router
 from Routes.DocumentRoutes import router as doc_router
+from fastapi.middleware.cors import CORSMiddleware
 from Routes import StaffRoutes
 
 server = FastAPI()
+
+
+server.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 server.include_router(router)
 server.include_router(doc_router)
 server.include_router(StaffRoutes.router)
