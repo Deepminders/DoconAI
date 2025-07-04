6e0a134ad26d95e65ca5c1cc393e7accd71d4386
Sehara2002 <shehara1010@gmail.com>
2025-07-04 18:12:11 +0530

    Front end Renew

--- Frontend/docon-ai/src/app/Components/Docs/Sidebar/DocSideBar.jsx
+++ Frontend/docon-ai/src/app/Components/Docs/Sidebar/DocSideBar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const DocSideBar = () => {
   return (
     <>
-        <div className="flex flex-col w-1/4 h-screen bg-gray-100 p-4"></div>
+        <div className="flex flex-col w-1/4 h-screen bg-gray-100 p-4">
+          
+        </div>
     </>
   )
 }
